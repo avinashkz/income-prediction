d16d83422808c1a73dcb2ffa24b4a49c9023ee68
ziavi7 <zi.avi7@gmail.com>
2017-11-25 19:59:55 -0800

    initialised Rscript to run from command line

--- src/data_processing.R
+++ src/data_processing.R
@@ -1,7 +1,19 @@
+#! /usr/bin/env Rscript 
+# data-processing.R
+# Avinash Prabhakaran, Nov 2016
+#
+# This script reads the raw data from the data folder 
+# and adds heading for both the train and test
+# data sets.
+#
+# Usage: Rscript data-processing.R
+
 library(tidyverse)
 
-train <-  read_csv(file = "../data/train.csv", col_names = FALSE)
-test <-  read_csv(file = "../data/test.csv",skip = 1 ,col_names = FALSE)
+main <- function(){
+  
+train <-  read_csv(file = "../data/train.csv")
+test <-  read_csv(file = "../data/test.csv")
 
 headings <- c("age","workclass", "fnlwgt", "education", "age", "maritalStatus",
               "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss",
@@ -12,3 +24,8 @@ names(test) <- headings
 
 write_csv(train, path = "../docs/train.csv")
 write_csv(test, path = "../docs/test.csv")
+
+}
+
+# call main function
+main()
--- src/read_data.R
+++ src/read_data.R
@@ -1,7 +1,24 @@
+#! /usr/bin/env Rscript 
+# read_data.R
+# Avinash Prabhakaran, Nov 2016
+#
+# This script reads the raw data from the UCI 
+# repository.
+#
+# Usage: Rscript read_data.R
+
+
 library(tidyverse)
+
+main <- function(){
 
 train <-  read_csv(file = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", col_names = FALSE)
 test <-  read_csv(file = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test",skip = 1 ,col_names = FALSE)
 
 write_csv(train, path = "../data/train.csv")
 write_csv(test, path = "../data/test.csv")
+
+}
+
+# call main function
+main()
