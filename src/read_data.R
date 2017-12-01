e0b8acc4bf15f55fd3d34b85a33798bc962477d3
ziavi7 <zi.avi7@gmail.com>
2017-11-30 19:13:47 -0800

    modified read_data script to take url as arguments

--- src/read_data.R
+++ src/read_data.R
@@ -7,17 +7,30 @@
 #
 # Usage: Rscript read_data.R
 
+args = commandArgs(trailingOnly = TRUE)
+link_test <- args[1]
+link_train <- args[2]
+#Read the location where you want to save the file and it will go and save in that location as test.csv and train.csv
+file_path <- args[3]
 
 library(tidyverse)
 
 main <- function(){
-
-train <-  read_csv(file = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", col_names = FALSE)
-test <-  read_csv(file = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test",skip = 1 ,col_names = FALSE)
-
-write_csv(train, path = "data/train.csv")
-write_csv(test, path = "data/test.csv")
-
+  
+  #read the files using the links from args
+  train <-  read_csv(file = link_train, col_names = FALSE)
+  test <-  read_csv(file = link_test,skip = 1 ,col_names = FALSE)
+  
+  #paste the path to where the files are to be saved
+  save_train <- paste(file_path, "/train.csv", sep = "")
+  save_test <- paste(file_path, "/test.csv", sep = "")
+  
+  #Save the processed train data
+  write_csv(train, path = save_train)
+  
+  #Save the processed test data
+  write_csv(test, path = save_test)
+  
 }
 
 # call main function
