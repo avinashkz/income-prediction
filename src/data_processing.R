e76b37ce2067395f9215f8df9196ee0fa63880b4
ziavi7 <zi.avi7@gmail.com>
2017-12-07 19:01:26 -0800

    Capitalized headings

--- src/data_processing.R
+++ src/data_processing.R
@@ -9,10 +9,8 @@
 # Usage: Rscript data-processing.R
 
 args = commandArgs(trailingOnly = TRUE)
-#Read the folder location where the data is stored in and the script will read in the test.csv and train.csv from that 
-read_path <- args[1]
 #Read the location where you want to save the pocessed data and it will go and save in that location as test.csv and train.csv
-write_path <- args[2]
+write_path <- args[1]
 
 
 library(tidyverse)
@@ -20,24 +18,24 @@ library(tidyverse)
 main <- function(){
   
   #creating the path for where raw data is stored
-  load_train <- paste(write_path, "/train.csv", sep = "")
-  load_test <- paste(write_path, "/test.csv", sep = "")  
+  load_train <- "data/train.csv"
+  load_test <- "data/test.csv" 
   
   #loading raw dara
   train <-  read_csv(file = load_train)
   test <-  read_csv(file = load_test)
   
   #Initializing headers
-  headings <- c("age","workclass", "fnlwgt", "education", "educationNum", "maritalStatus",
-                "occupation", "relationship", "race", "sex", "capitalGain", "capitalLoss",
-                "hoursPerWeek", "nativeCountry", "class")
+  headings <- c("Age","Work_Class", "Final_Weight", "Education", "Education_Num", "Marital_Status",
+                "Occupation", "Relationship", "Race", "Sex", "Capital_Gain", "Capital_Loss",
+                "Hours_Per_Week", "Native_Country", "Class")
   
   #Adding headers
   names(train) <- headings
   names(test) <- headings
   
-  train <- train %>% mutate(k50 = if_else(class == ">50K",true = 1, false = 0))
-  test <- test %>% mutate(k50 = if_else(class == ">50K",true = 1, false = 0))
+  train <- train %>% mutate(k50 = if_else(class == "Salary_Class",true = 1, false = 0))
+  test <- test %>% mutate(k50 = if_else(class == "Salary_Class",true = 1, false = 0))
   
   #creating path for saving the file
   save_train <- paste(write_path, "/train.csv", sep = "")
