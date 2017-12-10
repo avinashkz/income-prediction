f2822074a16afa1a87ac50b487e11b50164b7059
ziavi7 <zi.avi7@gmail.com>
2017-12-10 02:11:45 -0800

    updated arguments to optional

--- src/data_viz.R
+++ src/data_viz.R
@@ -6,12 +6,22 @@
 #
 # Usage: Rscript data_viz.R read_path write_path
 
-args = commandArgs(trailingOnly = TRUE)
+#args = commandArgs(trailingOnly = TRUE)
 #Read the folder location where the data is stored in and the script will read in the train.csv from that folder.
 #read_path <- "doc"
 #write_path <- "doc"
-read_path <- args[1]
-write_path <- args[2]
+#read_path <- args[1]
+#write_path <- args[2]
+
+library(optparse)
+option_list <- list(
+  make_option("--read", type = "character", default = "doc"),
+  make_option("--write", type = "character", default = "doc"))
+
+opt <- parse_args(OptionParser(option_list = option_list))
+
+read_path <- opt$read
+write_path <- opt$write
 
 library(tidyverse)
 library(forcats)
@@ -27,9 +37,24 @@ main <- function() {
   train <<-  read_csv(file = load_train) 
   
   g <- train %>% ggplot() +
-    geom_histogram(aes(Age, fill = Class), bins = 25, color = "black") + labs(title = "Histogram of Age", y = "Count") +
-    scale_fill_brewer(palette = "Set2") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+    geom_histogram(aes(Age, fill = Class), bins = 25, color = "black", alpha = 0.7) + 
+    labs(title = "Histogram for Age", y = "Count") +
+    scale_fill_manual(values = c("black","red")) + 
+    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16), 
+                       axis.text = element_text(size = 12), 
+                       axis.title = element_text(size = 14))
+  
   ggsave(filename = "age.png", path = "doc")
+  
+  q <- train %>% ggplot() +
+    geom_histogram(aes(Age, fill = Class), bins = 25, color = "black", position = "fill", alpha = 0.7) + 
+    labs(title = "Proportions for Age", y = "Proportion") +
+    scale_fill_manual(values = c("darkblue","orange")) + 
+    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16), 
+                       axis.text = element_text(size = 12), 
+                       axis.title = element_text(size = 14))
+  
+  ggsave(filename = "age_prop.png", path = "doc")  
   
   for (i in 1:(ncol(train)-2)){
     val <- variable_type(train[,i])
@@ -40,23 +65,52 @@ main <- function() {
 }
 
 categorical_plots <- function(x, i){
-  colnames(x) <- c("ax1", "class")
-  p <-  x %>% group_by(ax1, class) %>%
+  colnames(x) <- c("ax1", "Class")
+  p <-  x %>% group_by(ax1, Class) %>%
     summarise(count = n()) %>% ggplot() +
     geom_col(aes(x = fct_reorder(ax1,count, .desc = TRUE),
-                 y = count, fill = class), alpha = 0.7) +
+                 y = count, fill = Class), alpha = 0.7, color = "black") +
     xlab(colnames(train[,i])) + 
-    labs(title = paste("Count Plot of" ,colnames(train[,i])), y = "Count") +
-    scale_fill_brewer(palette = "Set2") +
-    theme_bw() + theme(plot.title = element_text(hjust = 0.5))
-    
+    labs(title = paste("Bar Plot for" ,colnames(train[,i])), y = "Count") +
+    scale_fill_manual(values = c("black","red")) +
+    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16), 
+                       axis.text = element_text(size = 12), 
+                       axis.title = element_text(size = 14))
+  
+  q <- x %>% group_by(ax1) %>% mutate(count = n()) %>% 
+    ggplot(aes(fct_reorder(ax1,count, .desc = TRUE))) +
+    geom_bar(aes(y = (..count..)/sum(..count..), fill = Class), position = "fill", color = "black", alpha = 0.7) +
+    xlab(colnames(train[,i])) +
+    labs(title = paste("Proportions for" ,colnames(train[,i])), y = "Proportion") +
+    scale_fill_manual(values = c("darkblue","orange")) +
+    theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 16), 
+                       axis.text = element_text(size = 12), 
+                       axis.title = element_text(size = 14))
+  
+
     if(n_distinct(train[,i]) > 3){
-     ggsave(plot = p + coord_flip(), filename = paste(tolower(colnames(train[,i])),".png",sep = ""), path = write_path)
+     ggsave(plot = p + coord_flip(),
+            filename = paste(tolower(colnames(train[,i])),".png",sep = ""),
+            path = write_path)
+      
+     ggsave(plot = q + coord_flip(),
+            filename = paste(tolower(colnames(train[,i])),"_prop.png",sep = ""),
+            path = write_path)
     }
     else
     {
-      ggsave(filename = paste(tolower(colnames(train[,i])),".png",sep = ""), path = write_path)
+      ggsave(plot = p,
+             filename = paste(tolower(colnames(train[,i])),".png",sep = ""),
+             path = write_path)
+      ggsave(plot = q,
+             filename = paste(tolower(colnames(train[,i])),"_prop.png",sep = ""),
+             path = write_path)
     }
+  
+
+  
+  
+  
 }
 
 variable_type <- function(x){
